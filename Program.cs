be7635c8ba80486ba3ed779ff0bd273342f6fc82
Vinicius Morais <vinidev.morais@gmail.com>
2024-01-12 19:17:38 -0300

    Revert "created program, finished project"
    This reverts commit e57111d29ba984b0170d5a5fa2e44da2950e6867.

--- Program.cs
+++ Program.cs
@@ -7,9 +7,7 @@ namespace Main
         static void Main (string[] args)
         {
             Account account = new Account (2032, "Alex", 5000.00);
-            BusinessAccount businesAccount = new BusinessAccount(350, "CIA", 20000.05, 9000000);
             System.Console.WriteLine(account);
-            System.Console.WriteLine(businesAccount);
         }
     }
 }
